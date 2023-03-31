aa98c81236ae771e5119d4bfe830598237a985fe
NisargPatel0711 <86000739+NisargPatel0711@users.noreply.github.com>
2023-03-31 15:56:07 -0400

    created components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewEmployeeComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
