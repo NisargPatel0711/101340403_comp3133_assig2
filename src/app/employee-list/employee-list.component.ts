1085074a28c853cfdd815ede5eefac0ddbe93154
NisargPatel0711 <86000739+NisargPatel0711@users.noreply.github.com>
2023-04-03 19:52:01 -0400

    showing employee details from database on employelist component & page

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Employee } from '../models/Employee';
+
+const get_employees = gql `query {
+  getEmployees {
+    id
+    first_name
+    last_name
+    email
+    gender
+    salary
+  }
+}
+`;
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
-
+export class EmployeeListComponent implements OnInit {
+  employees : Employee[] = []
+  title = '101340403_comp3133_assig2';
+  constructor (private apollo: Apollo){}
+  ngOnInit(): void {
+    this.apollo.watchQuery<any>({
+      query: get_employees
+    }).valueChanges
+    .subscribe(({data, loading}) => {
+      console.log(loading);
+      this.employees = data.getEmployees; 
+    })
+  }
+  
 }
