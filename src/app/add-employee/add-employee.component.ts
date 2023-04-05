26cb6c1e28b21aa92a3f091a47e2b3abe25978a8
NisargPatel0711 <86000739+NisargPatel0711@users.noreply.github.com>
2023-04-04 23:15:00 -0400

    edit employee completed

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -3,6 +3,7 @@ import { Apollo, gql } from 'apollo-angular';
 import { catchError, of } from 'rxjs';
 import { Employee } from '../models/Employee';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const add_employee = gql`
   mutation AddEmployee(
@@ -34,43 +35,116 @@ const add_employee = gql`
   }
 `;
 
+const edit_employee = gql`
+  mutation UpdateEmployee(
+    $updateEmployeeId: ID!
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $gender: String!
+    $salary: Float!
+  ) {
+    updateEmployee(
+      id: $updateEmployeeId
+      first_name: $firstName
+      last_name: $lastName
+      email: $email
+      gender: $gender
+      salary: $salary
+    ) {
+      error
+      message
+      status
+    }
+  }
+`;
+
+const get_employee_by_id = gql`
+  query GetEmployeeByID($getEmployeeByIdId: ID!) {
+    getEmployeeByID(id: $getEmployeeByIdId) {
+      id
+      first_name
+      last_name
+      email
+      gender
+      salary
+    }
+  }
+`;
+
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css'],
 })
-export class AddEmployeeComponent {
-  newEmployee: Employee[] = [];
-  error: any;
-  tempEmployee: any;
-
-  constructor(private apollo: Apollo, private router: Router) {}
-
-  onAddEmployeeButtonClick() {
-    this.addNewEmployee();
-  }
-
-  //add Listing
-  employeeForm: Employee = {
+export class AddEmployeeComponent implements OnInit {
+  form!: FormGroup;
+  employee: Employee = {
     id: '',
     first_name: '',
     last_name: '',
-    salary: 0,
-    gender: '',
     email: '',
+    gender: '',
+    salary: 0,
   };
+  error: any;
+  tempEmployee: any;
+  path = this.router.url;
+  id = this.path.replace('/edit/', '');
+  isEdit = this.path.includes('edit');
 
-  //addlicting
+  constructor(
+    private fb: FormBuilder,
+    private apollo: Apollo,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      gender: ['', Validators.required],
+      salary: ['', Validators.required],
+    });
+    if (this.isEdit) {
+      this.apollo
+        .watchQuery<any>({
+          query: get_employee_by_id,
+          variables: {
+            getEmployeeByIdId: this.id,
+          },
+        })
+        .valueChanges.subscribe(({ data, loading }) => {          
+          this.form.setValue({
+            first_name: data.getEmployeeByID.first_name,
+            last_name: data.getEmployeeByID.last_name,
+            email: data.getEmployeeByID.email,
+            gender: data.getEmployeeByID.gender,
+            salary: data.getEmployeeByID.salary,
+          });
+        });
+    }
+  }
+
+  onSubmit() {
+    if (this.isEdit) {
+      this.editEmployee(this.id);
+    } else if (this.isEdit == false) {
+      this.addNewEmployee();
+    }
+  }
+
   addNewEmployee() {
     this.apollo
       .mutate<any>({
         mutation: add_employee,
         variables: {
-          "firstName": this.employeeForm.first_name,
-          "lastName": this.employeeForm.last_name,
-          "email": this.employeeForm.email,
-          "gender": this.employeeForm.gender,
-          "salary": this.employeeForm.salary,
+          firstName: this.employee.first_name,
+          lastName: this.employee.last_name,
+          email: this.employee.email,
+          gender: this.employee.gender,
+          salary: this.employee.salary,
         },
       })
       .pipe(
@@ -82,9 +156,36 @@ export class AddEmployeeComponent {
         })
       )
       .subscribe((data) => {
-        this.tempEmployee = data;
-        this.newEmployee = this.tempEmployee.data.addEmployee;
-        console.log(this.newEmployee);
+        console.log(data);
+        // window.location.href = '/list';
+      });
+  }
+
+  editEmployee(id: string) {
+    console.log(this.form.value);
+    
+    this.apollo
+      .mutate<any>({
+        mutation: edit_employee,
+        variables: {
+          updateEmployeeId: id,
+          firstName: this.form.value.first_name,
+          lastName: this.form.value.last_name,
+          email: this.form.value.email,
+          gender: this.form.value.gender,
+          salary: this.form.value.salary,
+        },
+      })
+      .pipe(
+        catchError((error) => {
+          this.error = error.message;
+          if (error.networkError.status === 400)
+            this.error = 'Something went wrong (Bad Request)';
+          return of({ error: error });
+        })
+      )
+      .subscribe((data) => {
+        console.log(data);
         window.location.href = '/list';
       });
   }
