50091cf7b3b0bcdbfdab0ce10c4e1ccdb5026733
NisargPatel0711 <86000739+NisargPatel0711@users.noreply.github.com>
2023-04-06 14:15:09 -0400

    Login funtionality implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { AuthenticationGuard } from './authentication.guard';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -8,13 +9,13 @@ import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'list', pathMatch: 'full' },
-  {path: 'list', component:EmployeeListComponent},
-  {path: 'add', component:AddEmployeeComponent},
+  {path: 'list', component:EmployeeListComponent, canActivate: [AuthenticationGuard]},
+  {path: 'add', component:AddEmployeeComponent, canActivate: [AuthenticationGuard]},
   {path: 'login', component:LoginComponent},
   {path: 'signup', component:SignupComponent},
-  {path: 'view/:id', component:ViewEmployeeComponent},
-  {path: 'edit/:id', component:AddEmployeeComponent},
-  {path: '**', redirectTo: 'listings'}
+  {path: 'view/:id', component:ViewEmployeeComponent, canActivate: [AuthenticationGuard]},
+  {path: 'edit/:id', component:AddEmployeeComponent, canActivate: [AuthenticationGuard]},
+  {path: '**', redirectTo: 'list'}
 ];
 
 @NgModule({
